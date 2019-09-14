1f0fe58d9c84c2261ca207ea0a2223c18b8c4624
juslin03 <komenan.komenan@uvci.edu.ci>
2019-09-14 14:43:34 +0000

    init project

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Provider } from "react-redux";
+import logo from "./logo.svg";
+import "./App.css";
+import Posts from "./components/Posts";
+import Postform from "./components/Postform";
+
+import store from "./store";
+
+function App() {
+  return (
+    <Provider store={store}>
+      <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+       <h1 className='app-title'>Welcome to React</h1>
+      </header>
+      <Postform />
+      <hr />
+      <Posts />
+    </div>
+    </Provider>
+  );
+}
+
+export default App;
